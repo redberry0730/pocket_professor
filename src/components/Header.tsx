c90be30bf9535c84b1a09a9c5cf71beea134c6ca
Yeon Jae Cho (Skyler) <chonw@bc.edu>
2023-11-05 04:53:48 -0500

    UI edits 3
    UI edits 3.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+
 const Header: React.FC = () => (
-    <header style={{ 
-      backgroundColor: 'green', 
-      color: 'white', 
-      textAlign: 'center', 
-      padding: '1px',
-      fontSize: '20px',
-      marginBottom: '20px'
-       }}>
-      <img src="./prof.png" alt="Header Image" style={{ maxWidth: '100%' }} />
-      <h1>POCKET PROFESSOR</h1>
-    </header>
-  );
-  
-  export default Header;
+  <header style={{ 
+    backgroundColor: 'green', 
+    color: 'white', 
+    display: 'flex', // Use flex display to align items
+    justifyContent: 'center', // Center items horizontally
+    alignItems: 'center', // Center items vertically
+    fontSize: '20px',
+    marginBottom: '20px'
+  }}>
+    <img src="/images/prof.png" alt="Header Image" style={{ width: '50px', marginRight: '10px' }} />
+    <h1>POCKET PROFESSOR</h1>
+  </header>
+);
+
+export default Header;
