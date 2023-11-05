ad72cf8f880dd262629dc1b666ff078340196ddf
Yeon Jae Cho (Skyler) <chonw@bc.edu>
2023-11-05 11:39:18 -0500

    conflicts resolved.
    conflicts!!

--- src/components/AnswerBoxes.tsx
+++ src/components/AnswerBoxes.tsx
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-=======
 import React, { useEffect, useState } from 'react';
 
 // Define the props that this component will accept
 type AnswerBoxesProps = {
   extractedText: string;
 };
->>>>>>> 33a6a9e4f78f579e97d1a82560e3f0815f962ac8
 
 const AnswerBoxes: React.FC<AnswerBoxesProps> = ({ extractedText }) => {
   const [question, setQuestion] = useState('');
   const [userAnswer, setUserAnswer] = useState('');
   const [correctAnswer, setCorrectAnswer] = useState('');
   const [contentVisible, setContentVisible] = useState(false); // New state to manage visibility
-
 
   // Parse the JSON whenever the extractedText prop changes
   useEffect(() => {
@@ -32,13 +27,8 @@ const AnswerBoxes: React.FC<AnswerBoxesProps> = ({ extractedText }) => {
   }, [extractedText]);
 
   return (
-<<<<<<< HEAD
-    <div style={{}}>
-    <div id="answerBoxes" className="rounded-box" style={{backgroundColor:'lightgreen', padding: '10px', paddingBottom: '30px', borderRadius:'30px', marginTop:'20px', width:'750px', marginLeft:'570px'}}>
-=======
-    <div id="answerBoxes" className={`rounded-box ${contentVisible ? '' : 'hidden'}`}>
+    <div id="answerBoxes" className={`rounded-box ${contentVisible ? '' : 'hidden'}`} style={{backgroundColor:'lightgreen', padding: '10px', paddingBottom: '30px', borderRadius:'30px', marginTop:'20px', width:'750px', marginLeft:'570px'}}>
       {/* Content is always rendered, but visibility is controlled by contentVisible state */}
->>>>>>> 33a6a9e4f78f579e97d1a82560e3f0815f962ac8
       <div>
         <label style={{ fontWeight: 'bold', marginRight: '610px' }}>Question:</label>
         <br />
@@ -55,9 +45,7 @@ const AnswerBoxes: React.FC<AnswerBoxesProps> = ({ extractedText }) => {
         <textarea value={correctAnswer} onChange={(e) => setCorrectAnswer(e.target.value)} style={{ width: '700px', height: '150px' }} />
       </div>
     </div>
-    </div>
   );
 };
-
 
 export default AnswerBoxes;
