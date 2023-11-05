e10bc70de21fffbc3a7be9351138b9e8c15a1263
BostonCollegeDataVizLab <denged@bc.edu>
2023-11-05 08:50:20 -0500

    Resolved merge conflicts in UploadImg.tsx
    buh took way longer than I thought it would

--- src/components/UploadImg.tsx
+++ src/components/UploadImg.tsx
@@ -1,71 +1,57 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
-const UploadImage: React.FC = () => {
-  // Using the state variable names from HEAD
+const UploadImg: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [fileName, setFileName] = useState<string>('');
   const [extractedText, setExtractedText] = useState<string>('');
   const [isUploading, setIsUploading] = useState<boolean>(false);
-
-  // Using the file input ref from your friend's changes
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  // This handles file selection and clears the extracted text
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      setSelectedFile(event.target.files[0]);
+      const newFile = event.target.files[0];
+      setSelectedFile(newFile);
+      setFileName(newFile.name); // Display the file name
       setExtractedText(''); // Clear the previous extracted text
+      await handleUpload(newFile); // Trigger upload after file selection
     }
   };
 
-  // This handles the file upload process
-  const handleUpload = async () => {
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append('file', selectedFile);
-      setIsUploading(true);
+  const handleUpload = async (file: File) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    setIsUploading(true);
 
-      try {
-        const response = await axios.post('http://127.0.0.1:5000/upload', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        });
-        setExtractedText(response.data.result);
-      } catch (error) {
-        console.error('Error uploading file:', error);
-        setExtractedText('Error: Could not extract text. Please try again.');
-      } finally {
-        setIsUploading(false);
-      }
-    }
-  };
-
-  // This function triggers the file input when the button is clicked
-  const handleButtonClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      setExtractedText(response.data.result); // Ensure that response.data has a 'result' property
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      setExtractedText('Error: Could not extract text. Please try again.');
+    } finally {
+      setIsUploading(false);
     }
   };
 
   return (
     <div>
-      {/* Adding the label from your friend's changes */}
-      <label htmlFor="getFile" style={{ marginBottom: '10px' }}>Please upload an image (png/jpeg): </label>
-      {/* The file input is hidden, but can be triggered by the button click */}
+      <label>Please upload an image (png/jpeg): </label>
       <input
         type="file"
-        ref={fileInputRef}
-        id="getFile"
         onChange={handleFileChange}
         accept="image/png, image/jpeg"
+        ref={fileInputRef}
         style={{ display: 'none' }}
       />
-      {/* The button from your friend's changes, with the text change while uploading */}
-      <button onClick={handleButtonClick} disabled={isUploading} style={{marginBottom:'15px'}}>
-        {isUploading ? 'Uploading...' : 'Choose Image'}
+      <button onClick={() => fileInputRef.current?.click()} disabled={isUploading}>
+        {isUploading ? 'Uploading...' : 'Upload and Parse Image'}
       </button>
-      {/* Display the extracted text in a textarea */}
+      {fileName && <div>Selected file: {fileName}</div>}
       {extractedText && (
         <textarea
           value={extractedText}
@@ -77,4 +63,4 @@ const UploadImage: React.FC = () => {
   );
 };
 
-export default UploadImage;
+export default UploadImg;
